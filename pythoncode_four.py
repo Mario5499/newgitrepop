402f662a41da054d45e12023ac99e30c62a2c2c8
Mario <bashgular@protonmail.com>
2025-12-15 19:56:37 +0530

    Create pythoncode_four.py

---
+++ pythoncode_four.py
@@ -0,0 +1,28 @@
+import time
+import subprocess
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+
+options = webdriver.ChromeOptions()
+options.binary_location = "/usr/bin/ungoogled-chromium"
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--window-size=1345x610")
+driver = webdriver.Chrome(options=options)
+driver.set_window_size(1354, 610)
+actions = ActionChains(driver)
+print("script started")
+
+driver.get("https://rrkb.in.net/code/Pytho_code_4.html")
+time.sleep(5)
+
+textarea_element = driver.find_element("id", "lion")
+
+code = textarea_element.get_attribute('value')
+
+with open("pythoncode4.py", "w") as file:
+    file.write(code)
+driver.quit()
+result = subprocess.run(["python3", "pythoncode4.py"], capture_output=True, text=True)
+print("Output of pythoncode4.py:", result.stdout)
